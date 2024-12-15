b45f6a0fc8f9963eb987cd0f17f0260dc001f689
Ali masiu zama <152395908+DevOpsWithAlii@users.noreply.github.com>
2024-12-16 00:39:12 +0530

    Update hello.sh

--- day01/hello.sh
+++ day01/hello.sh
@@ -4,6 +4,6 @@
 
 echo "TWS: Hello Dosto"
 
-echo "Learners: DevOps Wale bhaiya , hum toh comment karenge"
+echo "Learners: DevOps Wale bhaiya , hum toh comment kaarenge"
 
 echo "TWS: Toh dosto, like bhi kar do"
